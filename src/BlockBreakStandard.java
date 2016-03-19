49bf8a07a75ef7cbf43e4154842221dbc47a03ba
John Ojala <johnojala@live.com>
2016-03-18 18:52:02 -0700

    Practice mode started can select levels

--- src/BlockBreakStandard.java
+++ src/BlockBreakStandard.java
@@ -30,7 +30,8 @@ public class BlockBreakStandard implements GameMode {
 		new String[] { "white_ui_controls", "media/sheet_white2x.png"},
 		new String[] { "nLevel", "media/gNextlevel.png" },
 		new String[] { "ex_game_screen", "media/game_screen.png"},
-		new String[] { "Text", "media/Mode_Text.png"}
+		new String[] { "Text", "media/Mode_Text.png"},
+		new String[] { "white_ui_controls" , "media/sheet_white1x.png" }
 	};
 	
 	private final int GameModeSelection = 0,
@@ -58,7 +59,12 @@ public class BlockBreakStandard implements GameMode {
 	private Sprite button_select[];
 	private Sprite ex_screen;
 	
-	public boolean gamePaused = false;
+	private boolean selectPractice = false;
+	private Sprite pracBox;
+	private Sprite[] pracArrows = new Sprite[2];
+	private int pracLevel = 1;
+	private int pracMax = 5;
+	
 	GameSounds moveClick; 
 	
 	public BlockBreakStandard() {
@@ -211,8 +217,6 @@ public class BlockBreakStandard implements GameMode {
 				new int[] { 640, 32 }
 			);
 		BlockStandardLevel.energyBar = localTexMap.get("energybar");
-		// BlockStandardLevel.pauseCursor
-		// BlockStandardLevel.numbers // numbers used for score display
 		int offset = 0;
 		for (int i = 0; i < BlockStandardLevel.numbers.length; i++) {
 			offset = i * 24 - 1;
@@ -223,7 +227,25 @@ public class BlockBreakStandard implements GameMode {
 					new int[] { 24, 30 }
 				);
 		}
-		
+		pracBox = new Sprite(
+				Global.textureMap.get("blue_ui"),
+				new int[] { 290, 94 },
+				new int[] { 49, 45 },
+				new int[] { 49, 45 }
+			);
+		pracArrows[0] = new Sprite(
+				localTexMap.get("white_ui_controls"),
+				//new int[] { 0, 251 },
+				new int[] { 0, 0 },
+				new int[] { 48, 48 },
+				new int[] { 48, 48 }
+			);
+		pracArrows[1] = new Sprite(
+				localTexMap.get("white_ui_controls"),
+				new int[] { 352, 351 },
+				new int[] { 48, 48 },
+				new int[] { 48, 48 }
+			);
 		// Update mode state when asset loading is completed
 		currentState = LoadState.LOADING_DONE;
 		return;
@@ -255,7 +277,15 @@ public class BlockBreakStandard implements GameMode {
 						case 2:
 							playLevel = new BlockStandardLevel03(localTexMap);
 							break;
+						case 3:
+							playLevel = new BlockStandardLevel04(localTexMap);
+							playLevel.level = 4;
+							break;
+						case 4:
+							playLevel = new BlockStandardLevel05(localTexMap);
+							playLevel.level = 5;
 						default:
+							playLevel = null;
 							break;
 					}
 				}
@@ -264,7 +294,11 @@ public class BlockBreakStandard implements GameMode {
 		} else {
 // @author Brock
 			GameSelector_background.draw(0, 0);
-			moveCursor();
+			if (selectPractice) {
+				inputPracMenu();
+			} else {
+				moveCursorMain();
+			}
 			optionBoxOffset = 50;
 			if (cursorPos == 0) {
 				//optionBox_2.draw(180 + optionBoxOffset, 180);
@@ -319,7 +353,12 @@ public class BlockBreakStandard implements GameMode {
 				//selector[1].draw(351 + optionBoxOffset, 187 + cursorPos * 70);
 			}
 			
+			// author John
+			if (selectPractice) {
+				drawPracticeSelect();
+			}
 		}
+		
 		if (pageBack) { cleanup(); }
 	}
 	
@@ -340,65 +379,110 @@ public class BlockBreakStandard implements GameMode {
 	/**
 	 * @author Brock
 	 */
-	public void moveCursor() {
+	private void moveCursorMain() {
 		if (movementInputDelay <= 0) {
-		if (Global.getControlActive(Global.GameControl.UP)) {
-			cursorPos--;
-			moveClick.playSoundEffect();
-			if (cursorPos < 0) {
-				
-				cursorPos = 3;
-			}
-			movementInputDelay = Global.inputReadDelayTimer;
-		}
-		if (Global.getControlActive(Global.GameControl.DOWN)) {
-			cursorPos++;
-			moveClick.playSoundEffect();
-			if (cursorPos > 3) {
-				cursorPos = 0;
-			}
-			movementInputDelay = Global.inputReadDelayTimer;
-		}
-		if (Global.getControlActive(Global.GameControl.CANCEL)) { // Cancel key moves the cursor to the program exit button
-			cursorPos = 3;
-				//BlockStandardLevel.gamePaused = false;
-			
-		}
-		if (Global.getControlActive(Global.GameControl.PAUSE)) {
-			//BlockStandardLevel.gamePaused = true;
-		}
-		if (Global.getControlActive(Global.GameControl.SELECT)) {
-			switch (cursorPos) {
-				case 0:
-					playLevel = new BlockStandardLevel01(localTexMap);
-					BlockStandardLevel.score = 0;
-					//activeGameMode = BlockMatchStandard;
-					break;
-				case 1:
-					playLevel = new BlockStandardLevelEx(localTexMap);
-					BlockStandardLevel.score = 0;
-					//activeGameMode = BlockMatchStandard;
-					break;
+			if (Global.getControlActive(Global.GameControl.UP)) {
+				cursorPos--;
+				moveClick.playSoundEffect();
+				if (cursorPos < 0) {
 					
-				case 2:
-					//game = 
-					//pageBack = true;
-					//activeGameMode = MainMenu;
-					//gameRunning = false;
-					break;
-				case 3:
-					pageBack = true;
-					break;
+					cursorPos = 3;
+				}
+				movementInputDelay = Global.inputReadDelayTimer;
 			}
-			
+			if (Global.getControlActive(Global.GameControl.DOWN)) {
+				cursorPos++;
+				moveClick.playSoundEffect();
+				if (cursorPos > 3) {
+					cursorPos = 0;
+				}
+				movementInputDelay = Global.inputReadDelayTimer;
+			}
+			if (Global.getControlActive(Global.GameControl.CANCEL)) { // Cancel key moves the cursor to the program exit button
+				cursorPos = 3;
+					//BlockStandardLevel.gamePaused = false;
+				
+			}
+			if (Global.getControlActive(Global.GameControl.PAUSE)) {
+				//BlockStandardLevel.gamePaused = true;
+			}
+			if (Global.getControlActive(Global.GameControl.SELECT)) {
+				switch (cursorPos) {
+					case 0: // normal mode
+						playLevel = new BlockStandardLevel01(localTexMap);
+						BlockStandardLevel.score = 0;
+						//activeGameMode = BlockMatchStandard;
+						break;
+					case 1: // practice mode
+						selectPractice = true;
+						break;
+					case 2: // high score
+						//game = 
+						//pageBack = true;
+						//activeGameMode = MainMenu;
+						//gameRunning = false;
+						break;
+					case 3: // exit
+					default:
+						pageBack = true;
+						break;
+				}
+				movementInputDelay = 2 * Global.inputReadDelayTimer;
+			}
+		} else if (movementInputDelay > 0) {
+			movementInputDelay -= Global.delta;
 		}
-	} else if (movementInputDelay > 0) {
-		movementInputDelay -= Global.delta;
 	}
-
+	
+	private void loadLevel(int levelID) {
+		switch (levelID) {
+			case 1:
+				playLevel = new BlockStandardLevel01(localTexMap);
+				break;
+			case 2:
+				playLevel = new BlockStandardLevel02(localTexMap);
+				break;
+			case 3:
+				playLevel = new BlockStandardLevel03(localTexMap);
+				break;
+			case 4:
+				playLevel = new BlockStandardLevel04(localTexMap);
+				break;
+			case 5:
+				playLevel = new BlockStandardLevel05(localTexMap);
+				break;
+			default:
+				Global.writeToLog("Attempting to load play level too high.", true);
+				break;
+		}
+	}
+		
+	private void inputPracMenu() {
+		movementInputDelay -= Global.delta;
+		if (movementInputDelay > 0) { return; }
+		if (Global.getControlActive(Global.GameControl.LEFT)) {
+			pracLevel--;
+			if (pracLevel < 1) { pracLevel = pracMax; }
+			movementInputDelay = Global.inputReadDelayTimer;
+		} else if (Global.getControlActive(Global.GameControl.RIGHT)) {
+			pracLevel++;
+			if (pracLevel > pracMax) { pracLevel = 1; }
+			movementInputDelay = Global.inputReadDelayTimer;
+		} else if (Global.getControlActive(Global.GameControl.SELECT)) {
+			loadLevel(pracLevel);
+		} else if (Global.getControlActive(Global.GameControl.CANCEL)) {
+			selectPractice = false;
+			return;
 		}
 		
-	
+	}
 
+	private void drawPracticeSelect() {
+		pracBox.draw(510, 250);
+		BlockStandardLevel.numbers[pracLevel].draw(525, 255);
+		//pracArrows[0].draw(470, 250);
+		//pracArrows[1].draw(560, 250);
+	}
+	
 }
 
---
+++ src/BlockStandardLevel05.java
@@ -0,0 +1,182 @@
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.newdawn.slick.opengl.Texture;
+/**
+ * Template level for simplify level-building.
+ * Copy the code here into a new BlockStandardLevel extended class and edit
+ * where necessary to set level difficulty. 
+ * @author John
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class BlockStandardLevel05 extends BlockStandardLevel {
+	private boolean specialActive = false;
+	private  BufferedReader infile;
+	
+	public BlockStandardLevel05(HashMap<String,Texture> rootTexMap) {
+		
+		// TODO: [CUSTOM] set background and user interface sprites
+		// if these sprite must be defined or the game will crash at runtime
+		background = new Sprite(
+				rootTexMap.get("bg_space_1"),
+				new int[] { 0, 0 },
+				new int[] { 1024, 768 },
+				new int[] { Global.glEnvWidth, Global.glEnvHeight }
+			);
+		
+		userInterface = new Sprite(
+				rootTexMap.get("ui_stdmode"), // default interface texture
+				new int[] { 0, 0 },
+				new int[] { 1024, 768 },
+				new int[] { Global.glEnvWidth, Global.glEnvHeight }
+			);
+		// TODO: [CUSTOM] set the score multiplier for the level
+		// default value: 1.0f
+		levelMultiplier = 1.5f;
+		
+		// TODO: [CUSTOM] set the energy gain multiplier. default value is 1.0f
+		energyGainMultiplier = 1.0f;
+		
+		// TODO: [CUSTOM] set the block size and grid size
+		// valid block dimensions are { 16, 32, 64 }
+		// valid grid sizes (respective to block size) are { 40, 20, 10 }
+		blockSize = new int[] { 32, 32 }; // default block size is { 32, 32 }
+		gridSize = new int[] { 20, 20 }; // default grid size is { 20, 20 }
+		// create the grid with x-dimension as specified above
+		grid = new GridColumn[gridSize[0]];
+		queue = new Block[gridSize[0]];
+		// build the grid according the level difficulty
+		buildGrid();
+		// set the grid draw starting position derived from grid and block size
+		gridBasePos = new int[] { 20, Global.glEnvHeight - blockSize[1] - 50 };
+		// set the cursor starting position in the center of the grid
+		cursorGridPos[0] = grid.length / 2;
+		cursorGridPos[1] = grid[0].blocks.length / 2;
+		
+		
+	}
+	
+	@Override
+	public void run() {
+		super.run();
+		// draw the grid and handle grid mechanics and input if the game is not paused
+		if (!gamePaused && !gameOver) {
+			processQueue();
+			energy -= Global.delta;
+			if (energy < 0) { energy = 0; }
+			if (energy > energyMax) { energy = energyMax; }
+			// draw the grid, return value indicates if there are blocks still falling from the last clear
+			gridMoving = drawGrid(500);
+			//shiftGrid();
+		
+			// for cursor surrounding block
+			cursor.draw(
+				gridBasePos[0] + blockSize[0] * cursorGridPos[0],
+				gridBasePos[1] - blockSize[1] * cursorGridPos[1],
+				blockSize
+			);
+			
+			if (specialActive) {
+				// if a special item or event has moved the selector cursor, handle that here
+				; 
+			} else if (!levelComplete) { // no special circumstance, handle input normally
+				if (inputDelay <= 0l) {
+					checkCommonControls();
+					// DEBUG: back out of the game to the main menu. not to be included in finished levels
+					if (Global.getControlActive(Global.GameControl.CANCEL)) {
+						levelFinished = true;
+						gameOver = true;
+					}
+				}
+			}
+		}
+		// draw the top-level UI frame, score and other elements
+		drawTopLevelUI();
+		drawEnergy();
+		if (gamePaused) {
+			// TODO: display the pause menu
+		} else if (gameOver) {
+			// TODO: show game over screen
+		}
+			
+	}
+
+	@Override
+	protected void buildGrid() {
+		Block b = null;
+		//dataType[] level;
+		
+		try {
+			String parseline;
+			String[] parseCSV;
+			infile = new BufferedReader(new FileReader("media/sp2.csv"));
+			parseline = infile.readLine();
+			int x=0;
+			while (parseline!= null) {
+				parseCSV = parseline.split(",");
+				grid[x] = new GridColumn(gridSize[1]);
+				for (int i = 0; i < parseCSV.length; i++) {
+					
+					grid[x].blocks[i] = new Block(Block.BlockType.BLOCK, Integer.parseInt(parseCSV[i]));
+				}
+				x++;
+				parseline = infile.readLine();
+			}
+			
+			infile.close();
+			//outfile.close();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// set the block count for the level
+		blocksRemaining = grid.length * grid[0].blocks.length;
+		// TODO: [CUSTOM] add any custom/special blocks that have limited generation (rocks, trash, wedge, etc.)
+		// remember to decrease blocksRemaining for each such block added 
+		
+	}
+	
+	@Override
+	protected Block getQueueBlock() {
+		Block b = null;
+		// TODO: [CUSTOM] define the type and rate of blocks that are added to the grid via the queue
+		b = new Block(Block.BlockType.BLOCK, Global.rand.nextInt(3));
+
+		return b;		
+	}
+
+	@Override
+	protected void processActivate() {
+		// TODO: score base value calculation is to be done within each case statement
+		// [CUSTOM] add case statements for each type of block that can be activated in the level
+		switch (grid[cursorGridPos[0]].blocks[cursorGridPos[1]].type) {
+			case BLOCK:
+				counter = checkGrid(cursorGridPos);
+				int adj = (int)Math.pow(counter - 1, 2);
+				updateScore(adj);
+				addEnergy(adj);
+				break;
+			default: // block does not activate, do nothing
+				break;
+		}
+		
+	}
+
+
+}
