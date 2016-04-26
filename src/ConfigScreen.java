078d59d7d5bd8460d6d7c31cea97b61e8004f4ac
johnojala <johnojala@live.com>
2016-04-26 10:21:57 -0700

    Added config screen class

---
+++ src/ConfigScreen.java
@@ -0,0 +1,28 @@
+
+public class ConfigScreen implements GameMode {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public LoadState getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
