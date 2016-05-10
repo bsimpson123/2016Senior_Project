fc497e23de4c78567c5ba7d4a7e0276311dc3f3f
John Ojala <johnojala@live.com>
2016-05-09 18:02:43 -0700

    Merge: AnimatedSprite class

--- src/AnimatedSprite.java
+++ src/AnimatedSprite.java
@@ -2,20 +2,41 @@ import static org.lwjgl.opengl.GL11.*;
 import org.newdawn.slick.opengl.Texture;
 
 public class AnimatedSprite extends Sprite {
+	/** The dimension of each cell within the texture. */
 	private final int[] cellSize;
+	/** The cell grid layout within the texture. */
 	private final int[] texCellLayout;
+	/*  */
 	private final int[] cellTexStartCoord;
+	/** The amount of negative (empty) space between each cell frame. */
 	private int[] cellFrameBuffer;
+	/** Identifies which cell in <code>texCellLayout</code> is the current frame. */
 	private int[] activeCell;
+	/** The amount of time to wait between each frame. */
 	private final long frameDelayTimer;
+	/** The time remaining for displaying the current frame. */
 	private long frameDelay;
+	/** The number of empty cells present in the last row of <code>texCellLayout</code>. */
 	private int emptyCells;
+	/*  */
 	private float[] cellDrawOffset;
-	private final boolean repeat;
+	/** The number of times to repeat the animation, -1 to repeat endlessly. */
+	private int repeatCount = -1;
+	/** Indicates if the animation has completed the required (limited) number of animation loops. */
 	private boolean finished = false;
+	/** OpenGL texture float offsets for drawing frames. */
 	private final int[] glDrawSpace;
 
-	public AnimatedSprite(Texture tex, int[] cellSize, int[] cellSetup, int frameDelayTime, boolean repeats) {
+	/**
+	 * 
+	 * @param tex <code>Texture</code> object containing the animation cells
+	 * @param cellSize The width and height of an individual cell as array
+	 * @param cellArangement The grid arrangement of the animation cells within the texture as [row count][column count]
+	 * @param frameDelayTime The amount of time (milliseconds) to wait between each frame
+	 * @param playTimes The number of times to play the animation sequence. Zero or negative values will cause
+	 * the animation to repeat forever.
+	 */
+	public AnimatedSprite(Texture tex, int[] cellSize, int[] cellArangement, int frameDelayTime, int playTimes) {
 		super(tex);		
 		glDrawSpace = new int[] { 
 			tex.getImageWidth(),
@@ -25,13 +46,13 @@ public class AnimatedSprite extends Sprite {
 		cellFrameBuffer = new int[] { 0, 0 };
 		frameDelayTimer = frameDelayTime;
 		frameDelay = frameDelayTimer;
-		repeat = repeats;
-		texCellLayout = cellSetup.clone();
+		repeatCount = playTimes;
+		texCellLayout = cellArangement.clone();
 		this.cellSize = cellSize.clone();
 		activeCell = new int[] { 0, 0 };
 	}
 	
-	public AnimatedSprite(Texture tex, int[] cellSize, int frameDelayTime, boolean repeats) {
+	public AnimatedSprite(Texture tex, int[] cellSize, int frameDelayTime, int playTimes) {
 		super(tex);		
 		glDrawSpace = new int[] { 
 			tex.getImageWidth(),
@@ -41,7 +62,7 @@ public class AnimatedSprite extends Sprite {
 		cellFrameBuffer = new int[] { 0, 0 };
 		frameDelayTimer = frameDelayTime;
 		frameDelay = frameDelayTimer;
-		repeat = repeats;
+		repeatCount = playTimes;
 		texCellLayout = new int[] {
 			( (int) tex.getHeight() ) / cellSize[0],
 			( (int) tex.getWidth() ) / cellSize[1]
@@ -66,6 +87,10 @@ public class AnimatedSprite extends Sprite {
 		if (activeCell[1] == texCellLayout[1]) {
 			activeCell[1] = 0;
 		} else if (activeCell[1] == (texCellLayout[1] - 1) && (activeCell[0] + emptyCells) == texCellLayout[0]) {
+			repeatCount -= 1;
+			if (repeatCount == 0) { // do not check for less than 1, negative values indicate repeat forever
+				finished = true;
+			}
 			activeCell[0] = 0;
 			activeCell[1] = 0;
 		}
@@ -73,6 +98,7 @@ public class AnimatedSprite extends Sprite {
 	
 	@Override
 	public void draw(int x, int y) {
+		if (finished) { return; }
 		glPushMatrix();
 		glMatrixMode(GL_MODELVIEW);
 		texture.bind();
@@ -84,7 +110,6 @@ public class AnimatedSprite extends Sprite {
 		float[] cellStop = new float[2];
 		cellStop[0] = cellStart[0] + ( (float) cellSize[0] ) / glDrawSpace[0];
 		cellStop[1] = cellStart[1] + ( (float) cellSize[1] ) / glDrawSpace[1];
-		
 		
 		// draw a quad textured to match the sprite
 		glBegin(GL_QUADS);
